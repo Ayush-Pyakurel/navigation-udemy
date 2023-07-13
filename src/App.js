634c95ca81f2e806899ab50d41824ec3e754a0b7
Ayush Pyakurel <ayushpyakurel449@gmail.com>
2023-07-13 13:37:04 +0545

    Install: className and react-icon library Added custom style to the button component Added the variation to the button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import Button from './Components/Button';
+import { GoBell } from 'react-icons/go';
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <div>
+        <Button success rounded outline>
+          <GoBell />
+          Payment Successful
+        </Button>
+      </div>
+      <div>
+        <Button danger outline>
+          Cancel the order
+        </Button>
+      </div>
+      <div>
+        <Button warning>See Deals</Button>
+      </div>
+      <div>
+        <Button secondary outline>
+          Hide Ads
+        </Button>
+      </div>
+      <div>
+        <Button primary outline rounded>
+          Hello
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
